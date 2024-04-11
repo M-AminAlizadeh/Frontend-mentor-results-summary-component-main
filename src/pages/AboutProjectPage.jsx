112681936a104f54b6f9cdc72f4661a85911d932
m-aminalizadeh <aminalizadeh@riseup.net>
2024-04-11 17:19:53 +0330

    Add about-project page

---
+++ src/pages/AboutProjectPage.jsx
@@ -0,0 +1,12 @@
+import Header from '../components/Header'
+
+function AboutProjectPage() {
+  return(
+    <>
+      <Header />
+      About project page
+    </>
+  )
+}
+
+export default AboutProjectPage;
