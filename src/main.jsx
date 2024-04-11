ab990eedc6f26d061a7c9a4f1e000d6b136b016e
m-aminalizadeh <aminalizadeh@riseup.net>
2024-04-11 14:44:35 +0330

    Add react-router to the project and delete the app.jsx file

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,21 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from 'react-router-dom'
+import HomePage from './pages/HomePage'
 import './index.css'
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <HomePage />
+  }
+])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
