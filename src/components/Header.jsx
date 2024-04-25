134b920f082dae72434e7e66984b657d2e4023f7
m-aminalizadeh <aminalizadeh@riseup.net>
2024-04-25 16:54:31 +0330

    Add logo to navbar and make it link to home page

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import Navbar from './Navbar';
 
 function Header() {
   return (
     <header className="border border-black flex justify-between p-3">
-      <h1>My logo</h1>
+      <Link to="/">
+        <img src="https://img.icons8.com/color/55/google-code.png" alt="google-code" />
+      </Link>
       <Navbar />
     </header>
   );
